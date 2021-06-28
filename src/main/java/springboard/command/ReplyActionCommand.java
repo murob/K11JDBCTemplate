0adb07ff887e9500998350d88acc0d998590f4c1
sung <59963039+ssh957@users.noreply.github.com>
2021-06-28 22:36:21 +0900

    commit

---
+++ src/main/java/springboard/command/ReplyActionCommand.java
@@ -0,0 +1,27 @@
+package springboard.command;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
+import model.JDBCTemplateDAO;
+import model.SpringBbsDTO;
+
+public class ReplyActionCommand implements BbsCommandImpl {
+	
+	@Override
+	public void execute(Model model) {
+		
+		//요청을 한번에 가져옴
+		Map<String, Object> paramMap = model.asMap();
+		HttpServletRequest req = (HttpServletRequest)paramMap.get("req");
+		
+		SpringBbsDTO dto = (SpringBbsDTO)paramMap.get("springBbsDTO");
+		
+		JDBCTemplateDAO dao = new JDBCTemplateDAO();
+		dao.reply(dto);
+	}
+
+}
---
+++ src/main/java/springboard/command/WriteActionCommand.java
@@ -0,0 +1,29 @@
+package springboard.command;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
+import model.JDBCTemplateDAO;
+import model.SpringBbsDTO;
+
+public class WriteActionCommand implements BbsCommandImpl {
+	
+	@Override
+	public void execute(Model model) {
+		
+		//model에 저장된 값을 Map컬렉션으로 변환한다.
+		Map<String, Object> paramMap = model.asMap();
+		//request객체와 DTO객체를 가져온다.
+		HttpServletRequest req = (HttpServletRequest)paramMap.get("req");
+		SpringBbsDTO springBbsDTO = (SpringBbsDTO)paramMap.get("springBbsDTO");
+		System.out.println("springBbsDTO.title="+springBbsDTO.getTitle());//폼값 확인용
+		//DAO객체 생성 후 쓰기 처리
+		JDBCTemplateDAO dao = new JDBCTemplateDAO();
+		
+		dao.write(springBbsDTO);
+		dao.close();
+	}
+}
